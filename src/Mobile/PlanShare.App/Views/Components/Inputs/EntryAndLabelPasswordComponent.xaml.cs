908b31ef9c2d0427e987d5de38aee1739ffae163
Neil Angelo <neil.angelo@hotmail.com>
2025-06-04 23:59:24 -0300

    Criando componente para Senha

---
+++ src/Mobile/PlanShare.App/Views/Components/Inputs/EntryAndLabelPasswordComponent.xaml.cs
@@ -0,0 +1,30 @@
+using System.Runtime.CompilerServices;
+
+namespace PlanShare.App.Views.Components.Inputs;
+
+public partial class EntryAndLabelPasswordComponent : ContentView
+{
+    public static readonly BindableProperty TitleProperty = BindableProperty
+        .Create(nameof(Title), typeof(string), typeof(EntryAndLabelPasswordComponent), string.Empty);
+
+    public string Title { get => (string)GetValue(TitleProperty); set => SetValue(TitleProperty, value); }
+
+    public EntryAndLabelPasswordComponent()
+	{
+		InitializeComponent();
+	}
+
+    private void ShowHidePassword(object sender, EventArgs e)
+    {
+        if(EntryPassword.IsPassword)
+        {
+            EntryPassword.IsPassword = false;
+            ImageEye.Source = "icon_eye.png";
+        }
+        else
+        {
+            EntryPassword.IsPassword = true;
+            ImageEye.Source = "icon_eye_hidden.png";
+        }
+    }
+}
