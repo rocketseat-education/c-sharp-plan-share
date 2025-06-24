149edbe77c3e3809ae879183cdbb77f1aa25204d
Neil Angelo <neil.angelo@hotmail.com>
2025-06-24 08:34:12 -0300

    Alterando Cor do Cursor e da Linha da Entry no Android

--- src/Mobile/PlanShare.App/Resources/Styles/Handlers/CustomEntryHandler.cs
+++ src/Mobile/PlanShare.App/Resources/Styles/Handlers/CustomEntryHandler.cs
@@ -1,13 +1,27 @@
-﻿namespace PlanShare.App.Resources.Styles.Handlers;
+﻿using PlanShare.App.Extensions;
+using Microsoft.Maui.Platform;
 
+namespace PlanShare.App.Resources.Styles.Handlers;
 
-class CustomEntryHandler
+public class CustomEntryHandler
 {
     public static void Customize()
     {
         Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("PlanShareEntry", (handler, entry) =>
         {
-           
+            var cursorColor = Application.Current!.GetPrimaryColor();
+            var lineColor = Application.Current!.GetLineColor();
+
+#if ANDROID
+               if(handler is not null
+               && handler.PlatformView is not null
+               && handler.PlatformView.TextCursorDrawable is not null
+               && handler.PlatformView.Background is not null)
+               {
+                   handler.PlatformView.TextCursorDrawable.SetTint(cursorColor.ToPlatform());
+                   handler.PlatformView.Background.SetTint(lineColor.ToPlatform());
+               }
+#endif
         });
     }
 }
