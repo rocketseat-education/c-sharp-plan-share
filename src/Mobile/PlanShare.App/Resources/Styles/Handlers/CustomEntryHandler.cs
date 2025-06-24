359e24e6c0275590f1629ea55e90ee4b16f4225a
Neil Angelo <neil.angelo@hotmail.com>
2025-06-24 08:09:08 -0300

    Criando e Entendendo os Hanlders

---
+++ src/Mobile/PlanShare.App/Resources/Styles/Handlers/CustomEntryHandler.cs
@@ -0,0 +1,13 @@
+﻿namespace PlanShare.App.Resources.Styles.Handlers;
+
+
+class CustomEntryHandler
+{
+    public static void Customize()
+    {
+        Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("PlanShareEntry", (handler, entry) =>
+        {
+           
+        });
+    }
+}
