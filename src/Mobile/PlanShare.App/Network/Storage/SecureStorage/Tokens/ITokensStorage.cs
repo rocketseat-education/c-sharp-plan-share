57b94474dfa38f2ff7dbafea19280b4b6f4b774e
Neil Angelo <neil.angelo@hotmail.com>
2025-07-20 13:00:04 -0300

    Implementando armazenamento no Secure Storage

---
+++ src/Mobile/PlanShare.App/Network/Storage/SecureStorage/Tokens/ITokensStorage.cs
@@ -0,0 +1,8 @@
+﻿namespace PlanShare.App.Network.Storage.SecureStorage.Tokens;
+
+public interface ITokensStorage
+{
+    Task Save(Models.ValueObjects.Tokens tokens);
+    Task<Models.ValueObjects.Tokens> Get();
+    void Clear();
+}
