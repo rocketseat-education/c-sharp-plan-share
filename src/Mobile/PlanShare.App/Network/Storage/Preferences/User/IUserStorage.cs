ed831a9232ffd47711335b91d35c24edd0c549c9
Neil Angelo <neil.angelo@hotmail.com>
2025-07-20 10:29:26 -0300

    Implementando armazenamento no Preferences

---
+++ src/Mobile/PlanShare.App/Network/Storage/Preferences/User/IUserStorage.cs
@@ -0,0 +1,10 @@
+﻿namespace PlanShare.App.Network.Storage.Preferences.User;
+
+public interface IUserStorage
+{
+    void Save(Models.ValueObjects.User user);
+    Models.ValueObjects.User Get();
+    bool IsLoggedIn();
+    void Clear();
+
+}
---
+++ src/Mobile/PlanShare.App/Network/Storage/Preferences/User/UserStorage.cs
@@ -0,0 +1,25 @@
+﻿namespace PlanShare.App.Network.Storage.Preferences.User;
+
+public class UserStorage : IUserStorage
+{
+    private const string ID_KEY = "id";
+    private const string NAME_KEY = "name";
+
+    public void Clear() => Microsoft.Maui.Storage.Preferences.Default.Clear();
+
+    public void Save(Models.ValueObjects.User user)
+    {
+        Microsoft.Maui.Storage.Preferences.Default.Set(ID_KEY, user.Id.ToString());
+        Microsoft.Maui.Storage.Preferences.Default.Set(NAME_KEY, user.Name);
+    }
+
+    public Models.ValueObjects.User Get()
+    {
+        var id = Microsoft.Maui.Storage.Preferences.Default.Get(ID_KEY, string.Empty);
+        var name = Microsoft.Maui.Storage.Preferences.Default.Get(NAME_KEY, string.Empty);
+
+        return new Models.ValueObjects.User(Guid.Parse(id), name);
+    }
+
+    public bool IsLoggedIn() => Microsoft.Maui.Storage.Preferences.Default.ContainsKey(ID_KEY);
+}
