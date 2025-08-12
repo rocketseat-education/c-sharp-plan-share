fe62cfcbc049e5a354b197ac01a15a1a6936538c
Neil Angelo <neil.angelo@hotmail.com>
2025-08-12 18:02:05 -0300

    Implementando Filtro para Custom Authorize Attribute

---
+++ src/Backend/PlanShare.Api/Filters/AuthenticatedUserFilters.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.IdentityModel.Tokens;
+using PlanShare.Communication.Responses;
+using PlanShare.Domain.Repositories.User;
+using PlanShare.Domain.Security.Tokens;
+using PlanShare.Exceptions;
+using PlanShare.Exceptions.ExceptionsBase;
+
+namespace PlanShare.Api.Filters;
+
+public class AuthenticatedUserFilter : IAsyncAuthorizationFilter
+{
+    private readonly IAccessTokenValidator _accessTokenValidator;
+
+    public AuthenticatedUserFilter(IAccessTokenValidator accessTokenValidator)
+    {
+        _accessTokenValidator = accessTokenValidator;
+    }
+
+    public async Task OnAuthorizationAsync(AuthorizationFilterContext context)
+    {
+      
+            var token = TokenOnRequest(context);
+            _accessTokenValidator.Validate(token);
+           
+    }
+
+    private static string TokenOnRequest(AuthorizationFilterContext context)
+    {
+        var authentication = context.HttpContext.Request.Headers.Authorization.ToString();
+        if (string.IsNullOrWhiteSpace(authentication))
+        {
+            throw new UnauthorizedException(ResourceMessagesException.NO_TOKEN);
+        }
+
+        return authentication["Bearer ".Length..].Trim();
+    }
+}
