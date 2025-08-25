e4618cf19eadbbf2ef21c9c203032d6cbf908d7e
Neil Angelo <neil.angelo@hotmail.com>
2025-08-25 19:44:06 -0300

    Introdução a Skeleton Loading

---
+++ src/Mobile/PlanShare.App/Views/Components/Skeletons/SkeletonView.cs
@@ -0,0 +1,13 @@
+﻿using PlanShare.App.Extensions;
+
+namespace PlanShare.App.Views.Components.Skeletons;
+public class SkeletonView : BoxView
+{
+
+    public SkeletonView()
+    {
+        Color = Application.Current!.GetSkeletonViewColor();
+
+        
+    }
+}
