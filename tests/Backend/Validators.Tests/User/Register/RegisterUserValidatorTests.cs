e67a696ae967cda3741d737754b35614af38c8a3
Neil Angelo <neil.angelo@hotmail.com>
2025-10-16 21:29:15 -0300

    Testes de Unidade para Validator Registrar User

---
+++ tests/Backend/Validators.Tests/User/Register/RegisterUserValidatorTests.cs
@@ -0,0 +1,38 @@
+﻿using CommonTestUtilities.Requests;
+using PlanShare.Application.UseCases.User.Register;
+using PlanShare.Exceptions;
+using Shouldly;
+
+namespace Validators.Tests.User.Register;
+public class RegisterUserValidatorTests
+{
+    [Fact]
+    public void Success()
+    {
+        var validator = new RegisterUserValidator();
+
+        var request = RequestRegisterUserBuilder.Build();
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeTrue();
+    }
+
+    [Fact]
+    public void Error_Name_Empty()
+    {
+        var validator = new RegisterUserValidator();
+
+        var request = RequestRegisterUserBuilder.Build();
+        request.Name = string.Empty;
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldSatisfyAllConditions(errors =>
+        {
+            errors.Count.ShouldBe(1);
+            errors.ShouldContain(error => error.ErrorMessage.Equals(ResourceMessagesException.NAME_EMPTY));
+        });
+    }
+}
