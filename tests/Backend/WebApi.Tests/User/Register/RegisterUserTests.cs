1e95c43e230813b9005ea3a146ac4514fac23e99
Neil Angelo <neil.angelo@hotmail.com>
2025-10-20 16:47:50 -0300

    Utilizando HostEnvironment na nossa API

--- tests/Backend/WebApi.Tests/User/Register/RegisterUserTests.cs
+++ tests/Backend/WebApi.Tests/User/Register/RegisterUserTests.cs
@@ -1,21 +1,14 @@
-﻿using Azure.Core;
-using CommonTestUtilities.Requests;
+﻿using CommonTestUtilities.Requests;
 using Microsoft.AspNetCore.Mvc.Testing;
-using PlanShare.Domain.Extensions;
-using PlanShare.Exceptions;
 using Shouldly;
-using System.Globalization;
 using System.Net;
-using System.Net.Http;
 using System.Net.Http.Json;
-using System.Text.Json;
-using WebApi.Tests.InlineData;
 
 namespace WebApi.Tests.User.Register;
 public class RegisterUserTests : IClassFixture<CustomWebApplicationFactory>
 {
     private readonly HttpClient _httpClient;
-    public RegisterUserTests(WebApplicationFactory<Program> factory)
+    public RegisterUserTests(CustomWebApplicationFactory factory)
     {
         _httpClient = factory.CreateClient();
     }
