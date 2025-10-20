7c7caa28cf2bd5d1e17a43896fe76bc347272ac4
Neil Angelo <neil.angelo@hotmail.com>
2025-10-20 18:11:35 -0300

    Testes de Erros para Registrar a conta de uma Pessoa

--- tests/Backend/WebApi.Tests/User/Register/RegisterUserTests.cs
+++ tests/Backend/WebApi.Tests/User/Register/RegisterUserTests.cs
@@ -1,13 +1,19 @@
 ﻿using CommonTestUtilities.Requests;
 using Microsoft.AspNetCore.Mvc.Testing;
+using PlanShare.Domain.Extensions;
+using PlanShare.Exceptions;
 using Shouldly;
+using System.Buffers.Text;
+using System.Globalization;
 using System.Net;
 using System.Net.Http.Json;
 using System.Text.Json;
+using WebApi.Tests.InlineData;
 
 namespace WebApi.Tests.User.Register;
 public class RegisterUserTests : IClassFixture<CustomWebApplicationFactory>
 {
+    private const string BaseUrl = "/users";
     private readonly HttpClient _httpClient;
     public RegisterUserTests(CustomWebApplicationFactory factory)
     {
@@ -18,7 +24,7 @@ public class RegisterUserTests : IClassFixture<CustomWebApplicationFactory>
     public async Task Success()
     {
         var request = RequestRegisterUserBuilder.Build();
-        var response = await _httpClient.PostAsJsonAsync("/users", request);
+        var response = await _httpClient.PostAsJsonAsync(BaseUrl, request);
         response.StatusCode.ShouldBe(HttpStatusCode.Created);
 
         using var responseBody = await response.Content.ReadAsStreamAsync();
@@ -28,5 +34,29 @@ public class RegisterUserTests : IClassFixture<CustomWebApplicationFactory>
         document.RootElement.GetProperty("id").GetGuid().ShouldNotBe(Guid.Empty);
         document.RootElement.GetProperty("name").GetString().ShouldBe(request.Name);
         document.RootElement.GetProperty("tokens").GetProperty("accessToken").GetString().ShouldNotBeNullOrEmpty();
+    }
+
+
+    public async Task Error_Name_Empty()
+    {
+        var request = RequestRegisterUserBuilder.Build();
+        request.Name = string.Empty;
+
+        var response = await _httpClient.PostAsJsonAsync(BaseUrl, request);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+
+        using var responseBody = await response.Content.ReadAsStreamAsync();
+
+        var document = await JsonDocument.ParseAsync(responseBody);
+
+        var errors = document.RootElement.GetProperty("errors").EnumerateArray();
+
+
+        errors.ShouldSatisfyAllConditions(erros =>
+        {
+            erros.Count().ShouldBe(1);
+            erros.ShouldContain(error => error.GetString().NotEmpty() && error.GetString()!.Equals(ResourceMessagesException.NAME_EMPTY));
+        });
     }
 }
