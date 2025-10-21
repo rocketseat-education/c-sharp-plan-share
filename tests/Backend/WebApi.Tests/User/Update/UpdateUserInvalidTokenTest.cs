cc63b6b3d66faf5c06d6d8b9d0b0751210e83cd1
Neil Angelo <neil.angelo@hotmail.com>
2025-10-20 21:57:30 -0300

    Finalizando Testes de Integração para Atualizar Perfil e Login

--- tests/Backend/WebApi.Tests/User/Update/UpdateUserInvalidTokenTest.cs
+++ tests/Backend/WebApi.Tests/User/Update/UpdateUserInvalidTokenTest.cs
@@ -1,34 +1,34 @@
-﻿//using CommonTestUtilities.Requests;
-//using Shouldly;
-//using System.Net;
+﻿using CommonTestUtilities.Requests;
+using Shouldly;
+using System.Net;
 
-//namespace WebApi.Tests.User.Update;
+namespace WebApi.Tests.User.Update;
 
-//public class UpdateUserInvalidTokenTest : CustomClassFixture
-//{
-//    private const string METHOD = "/users";
+public class UpdateUserInvalidTokenTest : CustomClassFixture
+{
+    private const string METHOD = "/users";
 
-//    public UpdateUserInvalidTokenTest(CustomWebApplicationFactory webApplication) : base(webApplication)
-//    {
-//    }
+    public UpdateUserInvalidTokenTest(CustomWebApplicationFactory webApplication) : base(webApplication)
+    {
+    }
 
-//    [Fact]
-//    public async Task Error_Token_Invalid()
-//    {
-//        var request = RequestUpdateUserBuilder.Build();
+    [Fact]
+    public async Task Error_Token_Invalid()
+    {
+        var request = RequestUpdateUserBuilder.Build();
 
-//        var response = await DoPut(METHOD, request, token: "tokenInvalid");
+        var response = await DoPut(METHOD, request, token: "tokenInvalid");
 
-//        response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
-//    }
+        response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+    }
 
-//    [Fact]
-//    public async Task Error_Without_Token()
-//    {
-//        var request = RequestUpdateUserBuilder.Build();
+    [Fact]
+    public async Task Error_Without_Token()
+    {
+        var request = RequestUpdateUserBuilder.Build();
 
-//        var response = await DoPut(METHOD, request, token: string.Empty);
+        var response = await DoPut(METHOD, request, token: string.Empty);
 
-//        response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
-//    }
-//}
+        response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+    }
+}
--- tests/Backend/WebApi.Tests/User/Update/UpdateUserTest.cs
+++ tests/Backend/WebApi.Tests/User/Update/UpdateUserTest.cs
@@ -1,59 +1,53 @@
-﻿//using CommonTestUtilities.Requests;
-//using PlanShare.Domain.Extensions;
-//using PlanShare.Exceptions;
-//using Shouldly;
-//using System.Globalization;
-//using System.Net;
-//using System.Text.Json;
-//using WebApi.Tests.InlineData;
-//using WebApi.Tests.Resources;
+﻿using CommonTestUtilities.Requests;
+using PlanShare.Domain.Extensions;
+using PlanShare.Exceptions;
+using Shouldly;
+using System.Globalization;
+using System.Net;
+using System.Text.Json;
+using WebApi.Tests.InlineData;
+using WebApi.Tests.Resources;
 
-//namespace WebApi.Tests.User.Update;
+namespace WebApi.Tests.User.Update;
 
-//public class UpdateUserTest : CustomClassFixture
-//{
-//    private const string BaseUrl = "/users";
+public class UpdateUserTest : CustomClassFixture
+{
+    private const string BaseUrl = "/users";
 
-//    private readonly UserIdentityManager _user;
+    private readonly UserIdentityManager _user;
 
-//    public UpdateUserTest(CustomWebApplicationFactory factory) : base(factory)
-//    {
-//        _user = factory.User;
-//    }
+    public UpdateUserTest(CustomWebApplicationFactory factory) : base(factory)
+    {
+        _user = factory.User;
+    }
 
-//    [Fact]
-//    public async Task Success()
-//    {
-//        var request = RequestUpdateUserBuilder.Build();
+    [Fact]
+    public async Task Success()
+    {
+        var request = RequestUpdateUserBuilder.Build();
+        var response = await DoPut(BaseUrl, request, _user.GetAccessToken());
+        response.StatusCode.ShouldBe(HttpStatusCode.NoContent);
+    }
 
-//        var response = await DoPut(BaseUrl, request, _user.GetAccessToken());
+    [Theory]
+    [ClassData(typeof(CultureInlineDataTest))]
+    public async Task Error_Empty_Name(string culture)
+    {
+        var request = RequestUpdateUserBuilder.Build();
+        request.Name = string.Empty;
 
-//        response.StatusCode.ShouldBe(HttpStatusCode.NoContent);
-//    }
+        var response = await DoPut(BaseUrl, request, _user.GetAccessToken(), culture);
+        response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+        await using var responseBody = await response.Content.ReadAsStreamAsync();
 
-//    [Theory]
-//    [ClassData(typeof(CultureInlineDataTest))]
-//    public async Task Error_Empty_Name(string culture)
-//    {
-//        var request = RequestUpdateUserBuilder.Build();
-//        request.Name = string.Empty;
+        var document = await JsonDocument.ParseAsync(responseBody);
+        var errors = document.RootElement.GetProperty("errors").EnumerateArray();
+        var expectedMessage = ResourceMessagesException.ResourceManager.GetString("NAME_EMPTY", new CultureInfo(culture));
 
-//        var response = await DoPut(BaseUrl, request, _user.GetAccessToken(), culture);
-
-//        response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
-
-//        await using var responseBody = await response.Content.ReadAsStreamAsync();
-
-//        var document = await JsonDocument.ParseAsync(responseBody);
-
-//        var errors = document.RootElement.GetProperty("errors").EnumerateArray();
-
-//        var expectedMessage = ResourceMessagesException.ResourceManager.GetString("NAME_EMPTY", new CultureInfo(culture));
-
-//        errors.ShouldSatisfyAllConditions(erros =>
-//        {
-//            erros.Count().ShouldBe(1);
-//            erros.ShouldContain(error => error.GetString().NotEmpty() && error.GetString()!.Equals(expectedMessage));
-//        });
-//    }
-//}
+        errors.ShouldSatisfyAllConditions(erros =>
+        {
+            erros.Count().ShouldBe(1);
+            erros.ShouldContain(error => error.GetString().NotEmpty() && error.GetString()!.Equals(expectedMessage));
+        });
+    }
+}
