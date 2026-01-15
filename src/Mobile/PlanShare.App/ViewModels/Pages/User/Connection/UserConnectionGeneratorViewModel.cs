629a0a23ee48e11d657d7f4ed5a4d90be3dfb82f
Neil Angelo <neil.angelo@hotmail.com>
2026-01-15 20:18:06 -0300

    Preparando pagina para exibir informações da pessoa que usou o codigo

--- src/Mobile/PlanShare.App/ViewModels/Pages/User/Connection/UserConnectionGeneratorViewModel.cs
+++ src/Mobile/PlanShare.App/ViewModels/Pages/User/Connection/UserConnectionGeneratorViewModel.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.SignalR.Client;
 using PlanShare.App.Data.Network.Api;
 using PlanShare.App.Models;
 using PlanShare.App.Navigation;
-using PlanShare.App.UseCases.Authentication.Refresh;
+using PlanShare.App.Resources;
 using PlanShare.Communication.Responses;
 
 namespace PlanShare.App.ViewModels.Pages.User.Connection;
@@ -17,19 +17,18 @@ public partial class UserConnectionGeneratorViewModel : ViewModelBase
     [ObservableProperty]
     public string connectionCode;
 
+    [ObservableProperty]
+    public JoinerUser joinerUser;
+
     private readonly HubConnection _connection;
 
-    // Codigo temporario
-    private readonly IUseRefreshTokenUseCase _useRefreshTokenUseCase;
-
     public UserConnectionGeneratorViewModel(
-        IUseRefreshTokenUseCase useRefreshTokenUseCase,
-
         IUserConnectionByCodeClient userConnectionByCodeClient,
         INavigationService navigationService) : base(navigationService)
     {
         _connection = userConnectionByCodeClient.CreateClient();
-        _useRefreshTokenUseCase = useRefreshTokenUseCase;
+
+        _connection.On<ResponseConnectingUserJson>("OnUserJoined", OnUserJoined);
     }
 
     [RelayCommand]
@@ -37,15 +36,59 @@ public partial class UserConnectionGeneratorViewModel : ViewModelBase
     {
         StatusPage = ConnectionByCodeStatusPage.GeneratingCode;
 
-        // Codigo Temporario
-        await _useRefreshTokenUseCase.Execute();
-
         await _connection.StartAsync();
 
         var result = await _connection.InvokeAsync<HubOperationResult<string>>("GenerateCode");
+        if (result.IsSuccess)
+        {
+            ConnectionCode = string.Join(' ', result.Response!.ToCharArray());
 
-        ConnectionCode = string.Join(' ', result.Response!.ToCharArray());
+            StatusPage = ConnectionByCodeStatusPage.WaitingForJoiner;
+        }
+        else
+        {
+            await _connection.StopAsync();
 
-        StatusPage = ConnectionByCodeStatusPage.WaitingForJoiner;
+            await _navigationService.ClosePage();
+
+            await _navigationService.ShowFailureFeedback(result.ErrorMessage);
+        }
+    }
+
+    [RelayCommand]
+    public async Task Cancel()
+    {
+        await _connection.InvokeAsync("Cancel", ConnectionCode.Replace(" ", string.Empty));
+
+        await _connection.StopAsync();
+
+        await _navigationService.ClosePage();
+    }
+
+    [RelayCommand]
+    public async Task Approve()
+    {
+        var result = await _connection.InvokeAsync<HubOperationResult<string>>("ConfirmCodeJoin", ConnectionCode.Replace(" ", string.Empty));
+        if (result.IsSuccess)
+            await _navigationService.ShowSuccessFeedback(string.Format(ResourceTexts.USER_JOINED_SUCCESSFULLY, JoinerUser.Name));
+        else
+            await _navigationService.ShowFailureFeedback(result.ErrorMessage);
+
+        await _connection.StopAsync();
+
+        await _navigationService.ClosePage();
+    }
+
+    private void OnUserJoined(ResponseConnectingUserJson response)
+    {
+        MainThread.BeginInvokeOnMainThread(() =>
+        {
+            JoinerUser = new JoinerUser
+            {
+                Name = response.Name
+            };
+
+            StatusPage = ConnectionByCodeStatusPage.JoinerConnectedPendingApproval;
+        });
     }
 }
